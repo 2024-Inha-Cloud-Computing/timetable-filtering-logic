d0edffdbd38465e11b0e42e22db2a8249946066b
RememberIOm <rememberiom@icloud.com>
2024-06-03 23:29:43 +0900

    entire_course.py 모듈 추가 및 테스트 코드 작성 후 main.py에 추가

---
+++ logic/processing/entire_course.py
@@ -0,0 +1,31 @@
+# 모든 강좌 데이터가 들어있는 DataFrame을 만드는 모듈
+
+import pandas as pd
+
+import import_csv
+
+# 과 별 강의 데이터를 모두 합쳐서 하나의 DataFrame으로 만드는 함수
+# input: DataFrame list
+# output: DataFrame
+def merge_all_data(data_list):
+    merged_data = pd.DataFrame()
+
+    # 모든 DataFrame을 하나로 병합
+    for data in data_list:
+        merged_data = pd.concat([merged_data, data])
+
+    # 학수번호를 기준으로 정렬
+    merged_data = merged_data.sort_values(by="course_class_id", ignore_index=True)
+
+    # 중복된 학수번호를 가진 행을 제거
+    merged_data = merged_data.drop_duplicates(subset="course_class_id", keep="first")
+    
+    return merged_data
+
+# 테스트 코드
+# RAW_PATH = "resource/raw"
+
+# imported_data = import_csv.import_csv(RAW_PATH)
+# merged_data = merge_all_data(imported_data)
+
+# print(merged_data)
--- logic/processing/main.py
+++ logic/processing/main.py
@@ -1,6 +1,7 @@
 # 과 별로 이루어진 강의 시간표를 메인 로직에서 사용하기 편하게 가공하는 모듈
 
 import import_csv
+import entire_course
 
 # import_csv 모듈 테스트 코드
 RAW_PATH = "resource/raw"
@@ -9,3 +10,11 @@ department_name_to_id, id_to_department_name = import_csv.get_department_dict(RA
 imported_data = import_csv.import_csv(RAW_PATH)
 
 print(imported_data[department_name_to_id["컴퓨터공학과-컴퓨터공학"]])
+
+# entire_course 모듈 테스트 코드
+RAW_PATH = "resource/raw"
+
+imported_data = import_csv.import_csv(RAW_PATH)
+merged_data = entire_course.merge_all_data(imported_data)
+
+print(merged_data)
