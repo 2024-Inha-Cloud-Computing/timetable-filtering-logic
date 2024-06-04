50db654f9442be1b6f427b07b9c40a244fd4a739
RememberIOm <rememberiom@icloud.com>
2024-06-04 21:34:53 +0900

    main.py 학과 인덱싱에 사용했던 dict, list csv 파일로 저장

--- logic/processing/main.py
+++ logic/processing/main.py
@@ -1,5 +1,7 @@
 # 학과 별 csv 파일을 메인 로직에서 사용하기 편하게 가공하는 모듈
 
+import pandas as pd
+
 import import_csv
 import entire_course
 import time_str_to_bit
@@ -8,10 +10,24 @@ import course_by_time
 RAW_PATH = "resource/raw"
 PROCESSED_PATH = "resource/processed"
 
-# [import_csv 모듈] 학과별 csv 파일 생성
+# [import_csv 모듈] 학과별 강의 & 인덱싱 csv 파일 생성
 department_name_to_id, department_id_to_name = import_csv.get_department_dict(RAW_PATH)
 df_list_by_departement = import_csv.import_csv(RAW_PATH)
 
+# department_name_to_id, department_id_to_name csv 파일 생성
+department_name_to_id_df = pd.DataFrame.from_dict(
+    department_name_to_id, orient="index", columns=["id"]
+)
+department_id_to_name_df = pd.DataFrame(department_id_to_name, columns=["department"])
+
+department_name_to_id_df.to_csv(
+    f"{PROCESSED_PATH}/department_index/department_name_to_id.csv"
+)
+department_id_to_name_df.to_csv(
+    f"{PROCESSED_PATH}/department_index/department_id_to_name.csv"
+)
+
+# 학과별 csv 파일 생성
 for department_id, df in enumerate(df_list_by_departement):
     department_name = department_id_to_name[department_id]
     df.to_csv(f"{PROCESSED_PATH}/import_csv/{department_name}.csv")
