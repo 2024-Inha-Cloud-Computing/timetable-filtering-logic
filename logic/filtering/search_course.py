7c4c997197581dbc495356cc0ba630ff5d4c2622
RememberIOm <rememberiom@icloud.com>
2024-06-08 13:50:10 +0900

    변환된 processed 데이터에 맞게 Filtering 인터페이스 수정

--- logic/filtering/search_course.py
+++ logic/filtering/search_course.py
@@ -2,8 +2,8 @@
 
 
 # 검색어를 입력받으면 해당 검색어를 포함하는 강의를 찾아주는 함수
-# input: 검색어, 강의 데이터
-# output: 검색어를 포함하는 강의 데이터
+# input: 검색어, 강의 DataFrame
+# output: 검색어를 포함하는 강의 DataFrame
 def search_course(search_word, course_df):
     # 학수번호, 강의명, 학과명, 교수명 중에서 검색어를 포함하는 강의를 찾아줌
     search_columns = ["course_id", "course_name", "professor"]
